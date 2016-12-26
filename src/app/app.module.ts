f80b9000269aadf78e7b6c795a3414298be0b061
SMahil <mahilsukh11@gmail.com>
2016-12-26 13:36:57 +0530

    bug: make header navigation responsive in mobile view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HttpModule }    from '@angular/http';
 import { DropdownModule } from 'ng2-dropdown';
 import { TabsModule } from 'ng2-bootstrap/components/tabs';
 import { ModalModule } from 'ng2-modal';
-import { TooltipModule } from 'ng2-bootstrap/components/tooltip';
+import { TooltipModule, CollapseModule } from 'ng2-bootstrap';
 
 // Imports for loading & configuring the in-memory web api
 // if not used will be removed for production by treeshaking
@@ -68,6 +68,7 @@ if (process.env.ENV == 'inmemory') {
     BoardModule,
     ChatModule,
     CodeModule,
+    CollapseModule,
     DashboardModule,
     DropdownModule,
     FormsModule,
@@ -92,6 +93,7 @@ if (process.env.ENV == 'inmemory') {
     BoardModule,
     ChatModule,
     CodeModule,
+    CollapseModule,
     DashboardModule,
     DropdownModule,
     FormsModule,
